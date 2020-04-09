951187e119bb5b10faf401be43263ed4a3e25f7f
Hojun_Kim <ghwns5338@gmail.com>
2020-04-10 00:22:29 +0900

    라이브러리 사용 고민
    날짜처리 moment.js

--- amzcalendar/src/App.js
+++ amzcalendar/src/App.js
@@ -2,50 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class AmazingCalendar extends React.Component{
+class App extends React.Component{
   constructor(props){
     super(props);
     this.state={
       year : null,
       month : null,
       week : null,
-      day : null,
+      today : null,
     } 
   };
 
   componentDidMount = () => {
-    let today = new Date();
-    let firstDay = new Date(today.getFullYear(), today.getMonth(),1);
-    let lastDay = new Date(today.getFullYear(), today.getMonth(),0);
+    let nowDate = new Date();
+    let today = nowDate.getDate();
+    let firstDay = new Date(nowDate.getFullYear(), nowDate.getMonth(),1);
+    let lastDay = new Date(nowDate.getFullYear(), nowDate.getMonth(),0);
+    let week = new Date(nowDate.getFullYear(), nowDate.getMonth(),nowDate.getDay());
+    
+    //아..moment 사용해야겠는걸
+    
+    this.setState({
+      today: this.today,
+    })
   }
 
   render(){
+    const { today } = this.state;
     return(
-      ""
+      <div>
+        {today}
+      </div>
     );
   }
 
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default App;
