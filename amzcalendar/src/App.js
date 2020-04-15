a291c9e9c29e16b33b050adda930cc77d6416037
Hojun_Kim <ghwns5338@gmail.com>
2020-04-15 21:02:20 +0900

    scss간단한 구성, 사용환경설정
    달력 월 변경 처리 필요 : redux 스토어, react-hooks 조사

--- amzcalendar/src/App.js
+++ amzcalendar/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import moment, { Moment as MomentTypes} from 'moment';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
